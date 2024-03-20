631b7685a81d36794c3c4bb82988a46d0284e0d7
Bea Serrano <beaserranolucio@gmail.com>
2024-03-20 21:37:20 +0100

    comunicacion hijo padre

---
+++ 03-REACT/Teoria/03-Comunicacion-entre-componentes/Hijo-a-Padre/src/components/ButtonCustom/ButtonCustom.jsx
@@ -0,0 +1,31 @@
+import "./ButtonCustom.css"
+
+//!-- COMUNICACIÓN DE PADRE A HIJO
+
+export const ButtonCustom = ({state, setState, textButton}) => {
+  return (
+        <button onClick={() => setState()}>
+          {textButton} {state}
+        </button>
+  )
+}
+
+/** las props que usamos en este componente son: 
+ * 
+ * state ---> estado del contador
+ * setState ---> es la función que maneja el contador
+ * textButton ---> texto que va dentro del botón
+ * 
+ * por último, usamos "value" como parámetro de la función setState
+ * y se refiere al valor actual del contador (del estado)
+ * 
+ * 
+ * POR QUÉ COMPONETIZAR ESTE BOTÓN
+ * este botón me sirve para cualquier estado cambiado, no solo para el contador
+ */
+
+//!-- aqui vemos una comunicación padre a hijo
+/** porque state y setState pertenecen al padre 
+ * ---> están declarados en App.jsx, que es el padre de ButtonCustom 
+ * 
+ * hay que enviar la info de este componente al padre, a App, para que la ejecute*/
